a591bd48b6bffc47817a42b4bfa899fb7437545b
hugdavid7 <hugdavid7@gmail.com>
2018-06-11 22:10:45 +0100

    confi CORS

--- configuration.js
+++ configuration.js
@@ -8,6 +8,16 @@ var DB_NAME = process.env.DB_NAME || '';
 var logLevel = intel.DEBUG;
 var serverPort = process.env.PORT || 3000;
 
+var crossdomainHeaders = {
+	'Access-Control-Allow-Origin' : function(request) {
+		return request.get('origin'); // Since we are using credentials, we can only return a single origin here! Use this to filter the allowed origins.
+	},
+	'Access-Control-Allow-Credentials' : true,
+    'Access-Control-Allow-Headers' : 'Origin, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Allow-Methods' : 'GET, OPTIONS, PUT, POST, DELETE',
+    'Access-Control-Max-Age': 86400
+};
+
 module.exports = {
 
     // Server configuration
@@ -25,6 +35,11 @@ module.exports = {
 
 	getMongoDBURI: function(){
 		return MONGODB_URI;
+	},
+
+	// Crossdomain
+	getCrossdomainHeaders: function() {
+		return crossdomainHeaders;
 	}
 
 };
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,28 @@
 var express = require('express');
+var rekuire = require('rekuire');
+var _ = require('lodash');
+
+var configuration = rekuire('configuration');
+var logger = rekuire('utils/LoggerProvider').getLogger();
 
 var routerAPI = express.Router();
+
+routerAPI.use(function(request, response, next) {
+    var crossdomainHeaders = configuration.getCrossdomainHeaders();
+
+    logger.debug('Configuring CORS');
+
+    _.each(crossdomainHeaders, function(value, key) {
+        if (_.isFunction(value)) {
+            value = value(request);
+        }
+
+        response.header(key, value);
+    });
+
+    if (request.method === 'OPTIONS') { response.end(); }
+    else { next(); }
+})
 
 routerAPI.use('/files', require('./fileexplorer'));
 
